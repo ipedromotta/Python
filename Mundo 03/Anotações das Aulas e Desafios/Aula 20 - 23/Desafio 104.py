189b06375fb0deac1ec05ba6cbcd5510a8c9ac38
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2021-01-03 13:21:55 -0300

    novo desafio

---
+++ Mundo 03/Anotações das Aulas e Desafios/Aula 20 - 23/Desafio 104.py
@@ -0,0 +1,20 @@
+"""
+EXERCÍCIO 104: Validando entrada de dados em Python.
+
+Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante a função input() do Python, só que
+fazendo a validação para aceitar apenas um valor numérico. Ex: n = leiaInt('Digite um n')
+"""
+def leiaInt(num):
+    while True:
+        valor = str(input((num)))
+        if valor.isnumeric():
+            return valor
+        else:
+            print('\033[31mERRO! Digite um número inteiro válido\033[m')
+        if valor.isnumeric():
+            break
+
+
+#programa principal
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o número {n}')
