d2bacff08a253240f8fc04f400e75d1787fa0685
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2021-01-07 16:47:09 -0300

    add e correcao

---
+++ Mundo 03/Exercícios Corrigidos/Exercício 108/Exercício 108.py
@@ -0,0 +1,6 @@
+"""
+EXERCÍCIO 108: Formatando moedas em Python.
+
+Adapte o código do desafio 107, criando uma função adicional chamada moeda() que consiga mostrar os valores como um
+valor monetário formatado.
+"""
