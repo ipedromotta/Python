5078fca603638da89ca3cd0243c2bdae5fa36593
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2021-01-08 10:14:04 -0300

    add/att

---
+++ Mundo 03/Exercícios Corrigidos/Exercício 109/Exercício 109.py
@@ -0,0 +1,6 @@
+"""
+EXERCÍCIO 109: Simplificando: Formatando moedas em Python.
+
+Modifique as funções que foram criadas no desafio 107 para que elas aceitem um parâmetro a mais, informando se o valor
+retornado por elas vai ser ou não formatado pela função moeda, desenvolvida no desafio 108.
+"""
