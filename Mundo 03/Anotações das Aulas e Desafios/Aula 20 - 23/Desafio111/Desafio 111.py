853ccdf40ecfc140a9d0ea70907f393d38cdb64b
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2021-01-11 11:51:35 -0300

    updates and new challenges

--- Mundo 03/Anotações das Aulas e Desafios/Aula 20 - 23/Desafio111/Desafio 111.py
+++ Mundo 03/Anotações das Aulas e Desafios/Aula 20 - 23/Desafio111/Desafio 111.py
@@ -5,7 +5,7 @@ Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados m
 Transfira todas as funções utilizadas nos desafios 107, 108 e 109 para o primeiro pacote e mantenha tudo funcionando.
 """
 
-from Desafio111 import moeda
+from Desafio111.utilidadescev import moeda
 
 p = float(input('Digite o preço: R$'))
 moeda.resumo(p)
--- Mundo 03/Exercícios Corrigidos/ex112/utilidadescev/moeda/__init__.py
+++ Mundo 03/Exercícios Corrigidos/ex112/utilidadescev/moeda/__init__.py
@@ -21,6 +21,7 @@ def metade(preço=0, formato=False):
 def moeda(preço=0, moeda='R$'):
     return f'{moeda}{preço:.2f}'.replace('.', ',')
 
+
 def resumo(preço=0, taxaa=10, taxar=5):
     print('-' * 30)
     print('RESUMO DO VALOR'.center(30))
@@ -29,5 +30,5 @@ def resumo(preço=0, taxaa=10, taxar=5):
     print(f'Dobro do preço: \t{dobro(preço, True)}')
     print(f'Metade do preço: \t{metade(preço, True)}')
     print(f'{taxaa}% de aumento: \t{aumentar(preço, taxaa, True)}')
-    print(f'{taxar}% de redução: \t\t{diminuir(preço, taxar, True)}')
+    print(f'{taxar}% de redução: \t{diminuir(preço, taxar, True)}')
     print('-' * 30)
