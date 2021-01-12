436eee4f7215c5ab659a3fb2496ab4c8ee410393
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2021-01-12 11:10:09 -0300

    updates and new challenges

---
+++ Mundo 03/Anotações das Aulas e Desafios/Aula 20 - 23/Desafio 113.py
@@ -0,0 +1,30 @@
+"""
+DESAFIO 113: Funções aprofundadas em Python
+
+Reescreva a função leiaInt() que fizemos no desafio 104, incluindo agora a possibilidade da digitação de um número de
+tipo inválido. Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
+"""
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except(ValueError, TypeError):
+            print('\033[31mERRO: Por favor, digite um número inteiro válido.\033[m')
+            continue
+        else:
+            return n
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except(ValueError, TypeError):
+            print('\033[31mERRO: Por favor, digite um número inteiro válido.\033[m')
+            continue
+        else:
+            return n
+
+
+n1 = leiaInt('Digite um número inteiro: ')
+n2 = leiaFloat('Digite um número real: ')
+print(f'O valor inteiro digitado foi {n1} e o real foi {n2}')
